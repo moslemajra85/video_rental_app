2e8f59b0d920c4179ed38992a19283aa261e762a
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2025-08-04 12:25:59 +0100

    mmovies

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -11,8 +11,37 @@ const getMovies = async (req, res) => {
   }
 };
 
-const getMovie = async (req, res) => {};
-const createMovie = async (req, res) => {};
+const getMovie = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const movie = await Movie.findById(id);
+
+    if (!movie) {
+      res.status(404).send("Movie not found");
+      return;
+    }
+
+    res.status(200).send(movie);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+const createMovie = async (req, res) => {
+  try {
+    let genre = await Genre.findById(req.body.genre);
+    if (!genre) {
+      return res.status(404).send("Genre not found");
+    }
+    const movie = new Movie({
+      ...req.body,
+    });
+
+    const result = await movie.save();
+    res.status(201).send(result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
 const updateMovie = async (req, res) => {};
 const deleteMovie = async (req, res) => {};
 
